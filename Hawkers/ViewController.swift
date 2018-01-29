773edeb97ea5f9616eff02a8b148eaf3597294b6
umd-akilah <akilahjackson@wireless-10-104-180-158.umd.edu>
2018-01-29 18:31:21 -0500

    testing the window

--- Hawkers/ViewController.swift
+++ Hawkers/ViewController.swift
@@ -9,19 +9,24 @@
 import Cocoa
 
 class ViewController: NSViewController {
+    
+ 
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
     }
-
-    override var representedObject: Any? {
-        didSet {
-        // Update the view, if already loaded.
-        }
+   
+    
+    @IBOutlet weak var hawkerslogo: NSButtonCell!
+    
+    
+    @IBAction func hawkerslogobtnrespons(_ sender: NSButtonCell) {
+        
+     
     }
-
-
+    
 }
 
