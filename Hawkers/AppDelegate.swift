1b9c262bde216a28673071ac3b26eb90f3ddb802
Akilah Jackson <akilahjackson@wireless-10-104-180-158.umd.edu>
2018-01-29 13:40:15 -0500

    Initial Commit

---
+++ Hawkers/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Hawkers
+//
+//  Created by Akilah Jackson on 1/29/18.
+//  Copyright © 2018 Akilah Jackson. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Hawkers/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Hawkers
+//
+//  Created by Akilah Jackson on 1/29/18.
+//  Copyright © 2018 Akilah Jackson. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
